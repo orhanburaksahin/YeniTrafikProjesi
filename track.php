5c8bda87e5b7b61d1dd2d7ab4859f1a743497fb3
orhanburaksahin <120124195+orhanburaksahin@users.noreply.github.com>
2026-02-12 01:36:39 +0300

    Test

--- track.php
+++
@@ -1,9 +0,0 @@
-<?php
-$input = json_decode(file_get_contents("php://input"), true);
-$file = "../config/logs.json";
-
-$logs = file_exists($file) ? json_decode(file_get_contents($file), true) : [];
-$logs[] = $input;
-
-file_put_contents($file, json_encode($logs, JSON_PRETTY_PRINT));
-http_response_code(200);
